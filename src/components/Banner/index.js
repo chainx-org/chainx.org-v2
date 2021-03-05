935e68213078cd9a382b6da04aa67ae09aa8e0d3
Bella-Liu9 <Bella_Liu20210301@163.com>
2021-03-05 12:05:00 +0800

    cut count-down package

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,13 +1,10 @@
-import React from 'react'
-import { IntlContextConsumer, useIntl } from "gatsby-plugin-intl"
+import React, { useState, useEffect } from 'react'
+import { IntlContextConsumer, useIntl } from 'gatsby-plugin-intl'
 import styled from 'styled-components'
-import Bannerlogo from './banner-logo.png'
 import BannerLogo from './bannerlogo.png'
-import ChainXWhitePaper from '../../assetpdf/ChainXWhitePaper.pdf'
-import ChainX from '../../assetpdf/ChainX.pdf'
+import ChainXWhitePaper from './ChainXWhitePaper.pdf'
+import ChainX from './ChainX.pdf'
 import BannerCutdown from './BannerCutdown'
-
-
 const StyledSection = styled.section`
   display: flex;
   justify-content: space-around;
@@ -165,38 +162,78 @@ const Bannerbgpic = styled.p`
     margin: 0 0 0 90px;
   }
 `
-export default function() {
 
+
+export default function () {
   const intl = useIntl()
 
   return (
     <StyledSection>
       <InnerSection>
-        
-          <IntlContextConsumer>
-            {({ languages, language: currentLocale }) => (
-              <Detail key="amache" className="ani animate__animated animate__fadeInLeft">
-                <Title>
-                  <p style={{display: ( "zh" === currentLocale ) ? "block" : "none"}}>让比特币更具价值</p>
-                  <p style={{display: ( "en" === currentLocale ) ? "block" : "none"}}>Make bitcoin more valuable</p>
-                </Title>
-                <Contents style={{display: ( "zh" === currentLocale ) ? "block" : "none"}}>ChainX 是波卡生态最早上线的项目，致力于 BTC Layer 2 拓展、数字资产网关及波卡二级中继链的开发研究，以实现跨链资产互通，引领比特币 Cross-Defi 新方向。</Contents>
-                <Contents style={{display: ( "en" === currentLocale ) ? "block" : "none"}}>ChainX, one of the earliest projects to launch in the Polkadot ecosystem, is committed to the research and application of Bitcoin Layer 2 expansion, its growth as a digital asset gateway and becoming a Polkadot second-layer relay chain for cross-chain asset exchange. A true pioneer in the development of Bitcoin Cross-DeFi.</Contents>
-                <div className="paperlink">
-                  <Linkbtn style={{display: ( "zh" === currentLocale ) ? "block" : "none"}} href={ChainX} className="txt txtzh" target="_blank" rel="noreferrer">
-                    白皮书
-                  </Linkbtn>
-                  <Linkbtn style={{display: ( "en" === currentLocale ) ? "block" : "none"}} href={ChainXWhitePaper} className="txt txten" target="_blank" rel="noreferrer">
-                    White Paper
-                  </Linkbtn>
-                </div>
-              </Detail>
-            )}
-          </IntlContextConsumer>
-        <div key="amache1" className="bglogo ani1 animate__animated animate__fadeInRight" >
+        <IntlContextConsumer>
+          {({ languages, language: currentLocale }) => (
+            <Detail
+              key="amache"
+              className="ani animate__animated animate__fadeInLeft"
+            >
+              <Title>
+                <p
+                  style={{ display: 'zh' === currentLocale ? 'block' : 'none' }}
+                >
+                  让比特币更具价值
+                </p>
+                <p
+                  style={{ display: 'en' === currentLocale ? 'block' : 'none' }}
+                >
+                  Make bitcoin more valuable
+                </p>
+              </Title>
+              <Contents
+                style={{ display: 'zh' === currentLocale ? 'block' : 'none' }}
+              >
+                ChainX 是波卡生态最早上线的项目，致力于 BTC Layer 2
+                拓展、数字资产网关及波卡二级中继链的开发研究，以实现跨链资产互通，引领比特币
+                Cross-Defi 新方向。
+              </Contents>
+              <Contents
+                style={{ display: 'en' === currentLocale ? 'block' : 'none' }}
+              >
+                ChainX, the earliest launched project in polkadot
+                ecosystem,ChainX is committed to the research and application of
+                Bitcoin layer 2 expansion, digital asset gateway and Polkadot
+                second-layer relay chain,To realize cross-chain asset exchange,
+                leading the new direction of Bitcoin Cross-DEFi.
+              </Contents>
+              <div className="paperlink">
+                <Linkbtn
+                  style={{ display: 'zh' === currentLocale ? 'block' : 'none' }}
+                  href={ChainX}
+                  className="txt txtzh"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  白皮书
+                </Linkbtn>
+                <Linkbtn
+                  style={{ display: 'en' === currentLocale ? 'block' : 'none' }}
+                  href={ChainXWhitePaper}
+                  className="txt txten"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  White Paper
+                </Linkbtn>
+              </div>
+            </Detail>
+          )}
+        </IntlContextConsumer>
+        <div
+          key="amache1"
+          className="bglogo ani1 animate__animated animate__fadeInRight"
+        >
           <Bannerbgpic />
         </div>
-        <BannerCutdown />
+        <BannerCutdown/>
       </InnerSection>
     </StyledSection>
   )
